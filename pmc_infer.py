dfa5d0e4b09eaa46aa4a5b75409631475a320856
Eric Bigelow <big@uber.com>
2019-08-27 20:30:15 -0700

    Metropolis-hastings working; all examples in Ch 3 match probmods.org

--- pmc_infer.py
+++ pmc_infer.py
@@ -1,24 +1,35 @@
+import collections
+import functools
 import random
 from six.moves import queue
-from tqdm import tqdm, tqdm_notebook
+from tqdm.auto import tqdm
 
 import torch
 from torch.autograd import Variable
 import pyro
+import pyro.distributions as dist
 from pyro import poutine
-from pyro.distributions import Uniform
 from pyro.infer.abstract_infer import TracePosterior
+
+
+def memoize(fn=None, **kwargs):
+    if fn is None:
+        return lambda _fn: memoize(_fn, **kwargs)
+    return functools.lru_cache(**kwargs)(fn)
 
 
 class EnumerateSearch(TracePosterior):
     """
-    Exact inference by enumerating over all possible executions.
+    Exact inference by enumerating over all possible executions.  
+
+    From RSA example.
     """
-    def __init__(self, model, max_tries=int(1e6), method='BFS', **kwargs):
+    def __init__(self, model, max_samples=1000, max_tries=int(1e6), method="BFS", **kwargs):
         self.model = model
+        self.max_samples = max_samples
         self.max_tries = max_tries
 
-        available_methods = ('BFS', 'DFS', 'best')
+        available_methods = ("BFS", "DFS", "best")
         if method not in available_methods:
             raise ValueError("Method must be one of: {}".format(avail_methods))
         self.search_method = method
@@ -26,9 +37,9 @@ class EnumerateSearch(TracePosterior):
         super(EnumerateSearch, self).__init__(**kwargs)
 
     def _traces(self, *args, **kwargs):
-        if self.search_method == 'BFS':
+        if self.search_method == "BFS":
             q = queue.LifoQueue()
-        elif self.search_method == 'DFS':
+        elif self.search_method == "DFS":
             q = queue.Queue()
         else:
             return self._traces_priority(*args, **kwargs)
@@ -36,9 +47,18 @@ class EnumerateSearch(TracePosterior):
         q.put(poutine.Trace())
         p = poutine.trace(
             poutine.queue(self.model, queue=q, max_tries=self.max_tries))
-        while not q.empty():
+        
+        n = 0
+        pbar = tqdm(total=self.max_samples)
+
+        while not q.empty() and n < self.max_samples:
+            n += 1
+            pbar.update(1)
+
             tr = p.get_trace(*args, **kwargs)
             yield tr, tr.log_prob_sum()
+
+        pbar.close()
 
 
     def _traces_priority(self, *args, **kwargs):
@@ -47,10 +67,10 @@ class EnumerateSearch(TracePosterior):
         # Add a little noise to priority queue to break ties
         q.put((torch.zeros(1).item() - torch.rand(1).item() * 1e-2, poutine.Trace()))
         q_fn = pqueue(self.model, queue=q)
-        for i in range(self.max_tries):
+        for i in tqdm(range(self.max_tries)):
             if q.empty():
                 break
-            tr = poutine.trace(q_fn).get_trace(*args, **kwargs)  # TODO should block
+            tr = poutine.trace(q_fn).get_trace(*args, **kwargs)  # XXX should block
             yield tr, tr.log_prob_sum()
 
 
@@ -72,24 +92,24 @@ class RejectionSampling(TracePosterior):
         super(RejectionSampling, self).__init__(**kwargs)
         
     def _traces(self, *args, **kwargs):
-        i = 0     # non-rejected samples
-        n = 0     # total number of samples
+        i = 0     # total number of samples
+        t = 0     # non-rejected samples
 
-        pbar = tqdm_notebook(total=self.num_samples)  # progress bar
+        pbar = tqdm(total=self.num_samples)  # progress bar
         
-        while i < self.num_samples:
-            n += 1
-            pbar.set_description('Total samples: {}'.format(n))
+        while t < self.num_samples:
+            i += 1
+            pbar.set_description("Total samples: {}".format(i))
 
             tr = poutine.trace(self.model).get_trace()
             tr.compute_log_prob()
             
             # If any observed nodes have log prob below threshold, reject this sample and continue
-            reject = any(tr.nodes[n]['log_prob_sum'] < self.reject_log_prob for n in tr.observation_nodes)
+            reject = any(tr.nodes[n]["log_prob_sum"] < self.reject_log_prob for n in tr.observation_nodes)
 
             # Otherwise, increment and return the sample
             if not reject:
-                i += 1
+                t += 1
                 pbar.update(1)
                 yield tr, tr.log_prob_sum()
 
@@ -98,9 +118,11 @@ class RejectionSampling(TracePosterior):
 
 class MH(TracePosterior):
     """
-    Initial implementation of MH MCMC
+    Simple implementation of Metropolis-Hastings algorithm.
 
-    https://github.com/pyro-ppl/pyro/blob/76097a8e0d9463c151a8590ec286fde99e5597ba/examples/storyboard/mh.py
+    - Adopted from: https://github.com/pyro-ppl/pyro/blob/
+        76097a8e0d9463c151a8590ec286fde99e5597ba/examples/storyboard/mh.py
+    - https://forum.pyro.ai/t/general-implementation-of-metropolis-hastings/255/2
     """
     def __init__(self, model, guide=None, proposal=None, samples=10, lag=1, burn=0):
         super(MH, self).__init__()
@@ -117,46 +139,61 @@ class MH(TracePosterior):
             self.guide = proposal
 
     def _traces(self, *args, **kwargs):
-        # initialize traces with a draw from the prior
-        old_model_trace = poutine.trace(self.model)(*args, **kwargs)
+        # Initialize traces with a draw from the prior
+        old_model_trace = poutine.trace(self.model, *args, **kwargs).get_trace()
+
         traces = []
         t = 0
         i = 0
-        while t < self.burn + self.lag * self.samples:
+
+        pbar = tqdm(total=self.samples)  # progress bar
+
+        while t < self.burn + (self.lag * self.samples):
             i += 1
+            pbar.set_description("Total samples: {}".format(i))
+
             # q(z' | z)
             new_guide_trace = poutine.block(
-                poutine.trace(self.guide))(old_model_trace, *args, **kwargs)
+                poutine.trace(self.guide).get_trace)(old_model_trace, *args, **kwargs)
+
             # p(x, z')
             new_model_trace = poutine.trace(
-                poutine.replay(self.model, new_guide_trace))(*args, **kwargs)
+                poutine.replay(self.model, new_guide_trace)).get_trace(*args, **kwargs)
+
             # q(z | z')
             old_guide_trace = poutine.block(
                 poutine.trace(
-                    poutine.replay(self.guide, old_model_trace)))(new_model_trace,
-                                                                  *args, **kwargs)
-            # p(x, z') q(z' | z) / p(x, z) q(z | z')
-            logr = new_model_trace.log_pdf() + new_guide_trace.log_pdf() - \
-                old_model_trace.log_pdf() - old_guide_trace.log_pdf()
-            rnd = pyro.sample("mh_step_{}".format(i),
-                              Uniform(pyro.zeros(1), pyro.ones(1)))
+                    poutine.replay(self.guide, old_model_trace)).get_trace)(new_model_trace, 
+                                                                            *args, **kwargs)
 
-            if torch.log(rnd).data[0] < logr.data[0]:
-                # accept
+            # p(x, z') q(z' | z) / p(x, z) q(z | z')
+            logr = new_model_trace.log_prob_sum() + new_guide_trace.log_prob_sum() - \
+                   old_model_trace.log_prob_sum() - old_guide_trace.log_prob_sum()
+
+            rnd = pyro.sample("mh_step_{}".format(i), dist.Uniform(0, 1))
+
+            if torch.log(rnd) < logr:  # Accept
                 t += 1
+                pbar.update(1)
+
                 old_model_trace = new_model_trace
                 if t <= self.burn or (t > self.burn and t % self.lag == 0):
-                    yield (new_model_trace, new_model_trace.log_pdf())
+                    yield (new_model_trace, new_model_trace.log_prob_sum())
+
+        pbar.close()
+
 
 # Single-site
 def single_site_proposal(model):
     def _fn(tr, *args, **kwargs):
-        choice_name = random.choice(
-            [s for s in tr.keys() if tr[s]["type"] == "sample"])
-        return pyro.sample(choice_name,
-                           tr[choice_name]["fn"],
-                           *tr[choice_name]["args"][0],
-                           **tr[choice_name]["args"][1])
+        sample_sites = [s for s in tr.nodes.keys() 
+                        if (tr.nodes[s]["type"] == "sample") and (s not in tr.observation_nodes)]
+
+        choice_name = random.choice(sample_sites)
+        return pyro.sample(choice_name, 
+                           tr.nodes[choice_name]["fn"], 
+                           *tr.nodes[choice_name]["args"], 
+                           **tr.nodes[choice_name]["kwargs"])
     return _fn
 
 
@@ -164,3 +201,127 @@ class SingleSiteMH(MH):
     def __init__(self, model, **kwargs):
         super(SingleSiteMH, self).__init__(
             model, guide=None, proposal=single_site_proposal(model), **kwargs)
+
+
+class HashingMarginal(dist.Distribution):
+    """
+    Inference in models that return a type that's not torch.Tensor; adopted from RSA example.
+
+    :param trace_dist: a TracePosterior instance representing a Monte Carlo posterior
+    """
+    def __init__(self, trace_dist, sites=None):
+        assert isinstance(trace_dist, TracePosterior), \
+            "trace_dist must be trace posterior distribution object"
+
+        if sites is None:
+            sites = "_RETURN"
+
+        assert isinstance(sites, (str, list)), \
+            "sites must be either '_RETURN' or list"
+
+        self.sites = sites
+        super(HashingMarginal, self).__init__()
+        self.trace_dist = trace_dist
+
+    has_enumerate_support = True
+
+    @memoize(maxsize=10)
+    def _dist_and_values(self):
+        # XXX currently this whole object is very inefficient
+        values_map, logits = collections.OrderedDict(), collections.OrderedDict()
+        for tr, logit in zip(self.trace_dist.exec_traces,
+                             self.trace_dist.log_weights):
+
+            # Get function return value
+            if isinstance(self.sites, str):
+                value = tr.nodes[self.sites]["value"]
+            else:
+                value = {site: tr.nodes[site]["value"] for site in self.sites}
+
+            if not torch.is_tensor(logit):
+                logit = torch.tensor(logit)
+
+            # Hash value so we can use it as a dict key
+            if torch.is_tensor(value):
+                value_hash = hash(value.cpu().contiguous().numpy().tobytes())
+            elif isinstance(value, dict):
+                value_hash = hash(self._dict_to_tuple(value))
+            else:
+                value_hash = hash(value)
+
+            # If value has already been seen  TODO
+            if value_hash in logits:
+                logits[value_hash] = dist.util.logsumexp(torch.stack([logits[value_hash], logit]), dim=-1)
+            else:
+                logits[value_hash] = logit
+                values_map[value_hash] = value
+
+        logits = torch.stack(list(logits.values())).contiguous().view(-1)
+        logits = logits - dist.util.logsumexp(logits, dim=-1)
+        d = dist.Categorical(logits=logits)
+        return d, values_map
+
+    def sample(self):
+        d, values_map = self._dist_and_values()
+        ix = d.sample()
+        return list(values_map.values())[ix]
+
+    def log_prob(self, val):
+        d, values_map = self._dist_and_values()
+        if torch.is_tensor(val):
+            value_hash = hash(val.cpu().contiguous().numpy().tobytes())
+        elif isinstance(val, dict):
+            value_hash = hash(self._dict_to_tuple(val))
+        else:
+            value_hash = hash(val)
+        return d.log_prob(torch.tensor([list(values_map.keys()).index(value_hash)]))
+
+    def enumerate_support(self):
+        d, values_map = self._dist_and_values()
+        return list(values_map.values())
+
+    def _dict_to_tuple(self, d):
+        """
+        Recursively converts a dictionary to a list of key-value tuples
+        Only intended for use as a helper function inside HashingMarginal!!
+        May break when keys cant be sorted, but that is not an expected use-case
+        """
+        if isinstance(d, dict):
+            return tuple([(k, self._dict_to_tuple(d[k])) for k in sorted(d.keys())])
+        else:
+            return d
+
+    def _weighted_mean(self, value, dim=0):
+        weights = self._log_weights.reshape([-1] + (value.dim() - 1) * [1])
+        max_weight = weights.max(dim=dim)[0]
+        relative_probs = (weights - max_weight).exp()
+        return (value * relative_probs).sum(dim=dim) / relative_probs.sum(dim=dim)
+
+    @property
+    def mean(self):
+        samples = torch.stack(list(self._dist_and_values()[1].values()))
+        return self._weighted_mean(samples)
+
+    @property
+    def variance(self):
+        samples = torch.stack(list(self._dist_and_values()[1].values()))
+        deviation_squared = torch.pow(samples - self.mean, 2)
+        return self._weighted_mean(deviation_squared)
+
+
+def EHMarginal(fn):
+    """Simple syntax for enumerated hashing marginal; from RSA example."""
+    return memoize(lambda *args: HashingMarginal(EnumerateSearch(fn).run(*args)))
+
+
+
+class SequentialMonteCarlo(TracePosterior):
+    """
+    TODO
+
+    - https://github.com/pyro-ppl/pyro/blob/dev/pyro/infer/smcfilter.py
+    - https://github.com/pyro-ppl/pyro/blob/dev/examples/smcfilter.py
+    """
+    pass
+
+
--- pmc_webppl.py
+++ pmc_webppl.py
@@ -8,48 +8,89 @@ import pyro.distributions as dist
 import pyro.infer.mcmc
 from pyro import poutine
 
-import functools
-import uuid
-from pmc_infer import EnumerateSearch, RejectionSampling, SingleSiteMH
+from pmc_infer import memoize, EnumerateSearch, RejectionSampling, SingleSiteMH, HashingMarginal
+
+
+def maybe_tensor(v):
+    return v if torch.is_tensor(v) else torch.Tensor([v])
+
 
 def flip(p=0.5):
     return torch.distributions.Bernoulli(p).sample()
 
+
 def pflip(name, p=0.5):
     return pyro.sample(name, dist.Bernoulli(p))
+
 
 def repeat(f, n):
     return [f() for _ in range(n)]
 
-def memoize(fn=None, **kwargs):
-    if fn is None:
-        return lambda _fn: memoize(_fn, **kwargs)
-    return functools.lru_cache(**kwargs)(fn)
 
 def factor(name, value):
     """
     Like factor in webPPL, adds a scalar weight to the log-probability of the trace
+
+    TODO: this is now a pyro built-in  
+          https://github.com/pyro-ppl/pyro/commit/86ea9051a6174acbb4c2f498bb486c41d464b5eb
     """
-    value = value if torch.is_tensor(value) else torch.Tensor([value])
+    value = maybe_tensor(value)
     d = dist.Bernoulli(logits=value)
     pyro.sample(name, d, obs=torch.ones((value.size() or 1)))
     
-def cond_var(name, value, normal_std=1e-15):
-    return pyro.sample(name, dist.Normal(torch.tensor(value), torch.tensor(normal_std)))
+def cond_var(name, value, normal_std=1e-7):
+    return pyro.sample(name, dist.Normal(torch.tensor(value, dtype=torch.float), torch.tensor(normal_std)))
     
-def condition(name, var, value=1, normal_std=1e-15):
-    pyro.sample(name, dist.Normal(torch.tensor(var), torch.tensor(normal_std)), 
-                obs=torch.tensor(value))
+def condition(name, var, value=1.0, normal_std=1e-7):
+    var = maybe_tensor(var)
+    normal_std = maybe_tensor(normal_std)
+    value = maybe_tensor(value)
+    pyro.sample(name, dist.Normal(var, normal_std), obs=value)
     
 def expectation(dist):
     return dist.mean
 
 
+def viz(data, to_type=(lambda v: v), plot_args={}, title=""):
+    """data is either Tensor/list of samples, or dist for which we enumerate samples and probs."""
+    if type(data) in (list, torch.Tensor):
+        # Histogram of samples
+        data = [to_type(d) for d in data]
+        plt.hist(data, weights=np.ones(len(data))/float(len(data)), **plot_args)
+
+    elif isinstance(data, pyro.distributions.Distribution):
+        # Barchart of distribution support
+
+        support = data.enumerate_support()
+        if type(support) is list:
+            support = torch.tensor(support, dtype=support[0].dtype)
+        else:
+            support = support.unique()
+
+        d = {to_type(s): float(data.log_prob(s).exp()) for s in support}
+        plt.bar(*zip(*d.items()), **plot_args)
+    else:
+        raise ValueError("data must be list of samples or pyro.distributions.Distribution")
+
+    plt.title(title)
+    plt.show()
+
+
+def viz_table(args):
+    # TODO
+    return
+
+def viz_scatter(data, args):
+    # TODO
+    return
+
 
 def Infer(model, 
-          posterior_method='enumerate', 
-          posterior_kwargs=dict(),
-          num_samples=1000,
+          posterior_method="enumerate", 
+          posterior_kwargs={},
+          marginal_method="empirical",
+          marginal_kwargs={},
+          num_samples=int(1e5),
           draw_samples=False):
     """
     Pyro imitation of WebPPL's Infer operator.
@@ -58,46 +99,37 @@ def Infer(model,
     - HMC (mostly done?)
     - SMC
     - incremental MH
-    - optimize: sgd, adagrad, rmsprop, adam
-    - variational (not in PMC or DIPPL)
+    - optimize (variational): sgd, adagrad, rmsprop, adam
 
     Ref: https://github.com/probmods/webppl/blob/dev/docs/inference/methods.rst
     """
-    method = posterior_method.lower()
-    if method == 'enumerate':
-        posterior = EnumerateSearch(model, max_tries=num_samples, **posterior_kwargs)       # method: 'DFS' | 'BFS' 
-    elif method in ('forward', 'importance'):
+    p_method = posterior_method.lower()
+    if p_method == "enumerate":
+        posterior = EnumerateSearch(model, max_samples=num_samples, **posterior_kwargs)       # method: "DFS" | "BFS" 
+    elif p_method in ("forward", "importance"):
         posterior = pyro.infer.Importance(model, num_samples=num_samples, **posterior_kwargs)
-    elif method == 'rejection':
+    elif p_method == "rejection":
         posterior = RejectionSampling(model, num_samples=num_samples, **posterior_kwargs)
-    elif method == 'mh':
+    elif p_method == "mh":
         posterior = SingleSiteMH(model, samples=num_samples, **posterior_kwargs)
-    elif method == 'hmc':
+    elif p_method == "hmc":
         # currently buggy
         kernel = pyro.infer.mcmc.HMC(model)
         posterior = pyro.infer.mcmc.MCMC(kernel, num_samples, **posterior_kwargs)  # warmup_steps, num_chains
     else:
         raise ValueError("Posterior method not defined for: {}".format(posterior_method))
+
+    posterior = posterior.run()
         
-    marginal = pyro.infer.EmpiricalMarginal(posterior.run())
+    m_method = marginal_method.lower()
+    if m_method == "empirical":
+        marginal = pyro.infer.EmpiricalMarginal(posterior, **marginal_kwargs)
+    elif m_method in ("hash", "hashing"):
+        marginal = HashingMarginal(posterior, **marginal_kwargs)
+    else:
+        raise ValueError("Posterior method not defined for: {}".format(marginal_method))
     
     if draw_samples:
         samples = marginal.sample(torch.Size([num_samples]))
         return samples
     return marginal
-
-def viz(data, to_type=(lambda v: v), plot_args={}, title=''):
-    if type(data) in (list, torch.Tensor):
-        # Histogram of samples
-        data = [to_type(d) for d in data]
-        plt.hist(data, weights=np.ones(len(data))/float(len(data)), **plot_args)
-
-    elif isinstance(data, pyro.distributions.Distribution):
-        # Barchart of distribution support
-        d = {to_type(s): float(data.log_prob(s).exp()) for s in data.enumerate_support()}
-        plt.bar(*zip(*d.items()), **plot_args)
-    else:
-        raise ValueError('data must be list of samples or pyro.distributions.Distribution')
-
-    plt.title(title)
-    plt.show()
